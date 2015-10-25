619edd6f90790f4f61324509a96dee6ef4484beb
lruiabc <568047669@qq.om>
2015-10-25 17:52:02 -0500

    added a all the file.

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git php mysql.client
+sudo apt-get install -y apache2 git php5 mysql-client
 
 git clone https://github.com/lruiabc/itmo-544-444-mp1.git
 
 mv ./../itmo-544-444-mp1/images /var/www/html/images
 mv ./../itmo-544-444-mp1/index.html /var/www/html
-mv ./../itmo-544-444-mp1/*.php /var/wwww/html
+mv ./../itmo-544-444-mp1/*.php /var/www/html
 
 
 echo "hello!" >/tmp/hello.txt
