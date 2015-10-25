c308f3b2683e50fbf1d4958f8a8e82e0858a4443
lruiabc <568047669@qq.om>
2015-10-25 02:40:00 -0500

    Added a new index.html form other location.

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,8 +3,6 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
-git clone https://github.com/lruiabc/itmo-544-444-mp1.git
-
 mv ./itmo-544-444-mp1/images /var/www/html/images
 mv ./itmo-544-444-mp1/index.html /var/www/html
 
