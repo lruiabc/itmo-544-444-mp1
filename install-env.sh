2e86424679a62a7ea2d44c8dd403acb4e8623f00
lruiabc <568047669@qq.om>
2015-10-25 04:00:50 -0500

    added a new picture.

--- install-env.sh
+++ install-env.sh
@@ -5,6 +5,6 @@ sudo apt-get install -y apache2 git
 
 git clone https://github.com/lruiabc/itmo-544-444-mp1.git
 
-mv ./itmo-544-444-mp1/images /var/www/html/images
+mv ./.../itmo-544-444-mp1/images /var/www/html/images
 
 echo "hello!" >/tmp/hello.txt
