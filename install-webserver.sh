bec578516ea8ab0d2e84c4a65eb73b28b93c5f6d
lruiabc <568047669@qq.om>
2015-10-25 16:31:40 -0500

    added a new index.html and new *php

--- install-webserver.sh
+++ install-webserver.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 sudo apt-get update -y
-sudo apt-get install -y apache2 git
+sudo apt-get install -y apache2 git php mysql.client
 
 git clone https://github.com/lruiabc/itmo-544-444-mp1.git
 
 mv ./../itmo-544-444-mp1/images /var/www/html/images
 mv ./../itmo-544-444-mp1/index.html /var/www/html
-
+mv ./../itmo-544-444-mp1/*.php /var/wwww/html
 
 
 echo "hello!" >/tmp/hello.txt
