4289212f179b6b01ca8fa9398c32462ef384d728
lruiabc <568047669@qq.om>
2015-10-25 03:43:44 -0500

    added a new index.html

--- install-webserver.sh
+++ install-webserver.sh
@@ -3,6 +3,8 @@
 sudo apt-get update -y
 sudo apt-get install -y apache2 git
 
+git clone https://github.com/lruiabc/itmo-544-444-mp1.git
+
 mv ./../itmo-544-444-mp1/images /var/www/html/images
 mv ./../itmo-544-444-mp1/index.html /var/www/html
 
