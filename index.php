233417608e87037a352ee62553639843c6c138be
lruiabc <568047669@qq.om>
2015-10-27 13:19:29 -0500

    added index.php

--- index.php
+++ index.php
@@ -5,10 +5,23 @@ print "Hello world!";
 ?>
 <html>
 <head>
-<title>
-best page ever</title>
+<title>Hello app</title>
 </head>
 <body>
+<form enctype="multipart/form-data" action="result.php" method="POST">
+	<input type="hidden" name="MAX_FILE_SIZE" value="30000000" />
+	send this file:<input name="userfile" type="file" /><br>
+Enter Email of user:<input type="email" name="useremail"><br>
+Enter Phone of user (1-XXX-XXX-XXXX):<input type="phone" name="phone">
+
+<input type="submit" value="Send File">
+</form>
+<hr />
+<form enctype="multipart/form-data" action="gallery'php" method="POST">
+Enter Email of user for gallery to browse:<input type="email" name="email">
+<imput type="submit" value="Load Gallery" />
+</form>
+
 hello<?php print "world"; ?>
 </body>
 </html>
