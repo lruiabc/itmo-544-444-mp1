a5434e726b6628b4e398a640653e2daf1fa2301f
lruiabc <568047669@qq.om>
2015-10-29 00:43:12 -0500

    added a result page from

--- result.php
+++ result.php
@@ -3,31 +3,30 @@
 session_start();
 //echo $_FILES;
 echo $_POST['useremail'];
+echo $_POST['phone'];
 $uploaddir='/tmp/';
 $uploadfile = $uploaddir . basename($_FILES['userfile']['name']);
-
 echo '<pre>';
 if(move_uploaded_file($_FILES['userfile']['tmp_name'],$uploadfile)){
 	echo "File is calid, and was successfully uploaded.\n";
 }else{
 	echo "Possible file upload attack!\n";
 }
-
 echo 'Here is some more debugging info:';
 print_r($_FILES);
-
 print "</pre>";
 require 'vendor/autoload.php';
-use Aws\s3\s3client;
-
-$client = S3lient::factory();
-$buchet = uniqid("php-jrh-",false);
-
+use Aws\S3\S3Client;
+$client = S3Client::factory(array(
+'version' =>'latest',
+'region'  => 'us-west-2'
+));
+$bucket = uniqid("php-jrh-",false);
 $result = $client->createBucket(array(
 	'Bucket'=> $bucket
 ));
-
-$client->waitUntiBucketExists(array('Bucket'=> $bucket));
+$client->waitUntil('BucketExists', array('Bucket' => $bucket));
+//$client->waitUntilBucketExists(array('Bucket' => $bucket));
 $key = $uploadfile;
 $result = $client->putObject(array(
 	'ACL'=>'public-read',
@@ -35,71 +34,57 @@ $result = $client->putObject(array(
 	'Key'=>$key,
 	'SourceFile'=> $uploadfile
 ));
-
 $url = $result['ObjectURL'];
 echo $url;
-
 use Aws\Rds\RdsClient;
 $client = RdsClient::factory(array(
-	'region' => 'us-west-2'
+'version' =>'latest',	
+'region' => 'us-west-2'
 ));
-
-$result = $client->describeDBInstances(array(
-	'DBInstanceIdentifier'=>'itmo544grh-mp1',
-));
-
-$endpoint = "";
-
-foreach ($result->getPath('DBInstances/*/Endpoint/Address')as $ep){
+//$result = $client->describeDBInstances(array(
+//	'DBInstanceIdentifier'=>'itmo544-ght-db',
+//));
+//$endpoint = "";
+//foreach ($result->getPath('DBInstances/*/Endpoint/Address')as $ep){
 	//do something with the meessage
-	echo "==========". $ep ."========";
-	$endpoint = $ep;
-}
-
+//	echo "==========". $ep ."========";
+//	$endpoint = $ep;
+//}
 	//echo "begin database";
-$link = mysqli_connect($endpoint,"rui","110224Fish","customerresech") or die("Error " . mysqli_error($link));
-
-//check connection
-if (mysqli_connect_error()){
-	printf("Connect failed: %s\n",mysqli_connect_error());
-	exit();
+//$link = mysqli_connect($endpoint,"guhaotian","909690ght","guhaotiandb") or die("Error " . mysqli_error($link));
+$link = mysqli_connect("itmo544-ght-db.c4bvwzwqz1hy.us-west-2.rds.amazonaws.com","guhaotian","909690ght","guhaotiandb") or die("Error " . mysqli_error($link));
+/* check connection */
+if (mysqli_connect_errno()) {
+    printf("Connect failed: %s\n", mysqli_connect_error());
+    exit();
 }
+//check connection
+//if (mysqli_connect_error()){
+//	printf("Connect failed: %s\n",mysqli_connect_error());
+//	exit();0
+//}
 //prepared statement, stage1:prepare
 if(!($stmt =$link->prepare("INSERT INTO items (id,email,phone,filename,s3rawurl,s3finishedurl,status,issubscribed)VALUES(NULL,?,?,?,?,?,?,?)"))){
 	echo "Prepare failed:(" . $link->errno . ")" . $link->error;
 }
-
 $email = $_POST['useremail'];
-$phone = $_post['phone'];
-$s3rawurl = $url;//$result['ObjectURL'];from above
+$phone = $_POST['phone'];
+$s3rawurl = $result['ObjectURL'];//;from above
 $filename = basename($_FILES['userfile']['name']);
 $s3finishedurl = "none";
 $status=0;
 $issubscribed=0;
-$stmt->bind_param("sssssii",email,$phone,$filename,$s3rawrul,$s3finishedurl,$status,$issbuscribed);
-
+$stmt->bind_param("sssssii",$email,$phone,$filename,$s3rawurl,$s3finishedurl,$status,$issubscribed);
 if(!$stmt->execute()){
 	echo "Execute failed:(" . $stmt->errno . ")" . $stmt->error;
 }
-
 printf("%d Row inserted.\n", $stmt->affected_rows);
-
 $stmt->close();
-
 $link->real_query("SELECT * FROM items");
 $res = $link->use_result();
-
 echo "Result set order...\n";
 while ($row = $res->fetch_assoc()){
 	echo $row['id'] . " " .$row['email']. " " .$row['phone'];
 }
-
 $link->close();
-
-
-
-
-
-
-
 ?>
